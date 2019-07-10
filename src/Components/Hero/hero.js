a425f331fa0164b2ab16da09c44eba26f8450cf8
CN0107 <cn0107@CN0107.lan>
2019-07-10 17:10:56 +0100

    more components

--- src/Components/Hero/hero.js
+++ src/Components/Hero/hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './hero.css'
+import Card from '../Card/card'
 
 
 const Hero = () => {
@@ -8,14 +9,7 @@ const Hero = () => {
             <div id='tagline'>
                 <h1 id='slogan'>Your Morning Trump</h1>
             </div>
-            <div id='dataimg'>
-                <img id='heroimg' src='https://www.veteranstoday.com/wp-content/uploads/2018/08/ScreenHunter-1147-1-696x547.jpg'
-                    alt='' />
-            </div>
-            <div id='snatch'>
-                <h2>LOOK WHAT TRUMP DID TODAY</h2>
-            </div>
-
+            <Card id='herocard' />
         </div>
     )
 }
