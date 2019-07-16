020bba5890ceec4af141268261eb8a7809e22b08
CN0105 <cn0105@CN0105.lan>
2019-07-16 12:22:00 +0100

    Updates

---
+++ precache-manifest.8f158150693ef83c85e1753c6fd18ebc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1eaa058c94a2ec99e3bf06a095f531f",
+    "url": "/reactNewsApp/index.html"
+  },
+  {
+    "revision": "656a9dbf020098eb998e",
+    "url": "/reactNewsApp/static/css/main.ebbb2f3a.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/reactNewsApp/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "656a9dbf020098eb998e",
+    "url": "/reactNewsApp/static/js/main.203b99bc.chunk.js"
+  },
+  {
+    "revision": "0199ccb89bd6d486f924",
+    "url": "/reactNewsApp/static/js/runtime~main.5e56cdd6.js"
+  }
+]);
